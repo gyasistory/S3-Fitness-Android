ce9dded9b2c1f17e4352e47b0937f7dec4dd9c1e
Gyasi Story <gyasi.story@cru.org>
2020-04-27 18:28:38 -0400

    Add Navigation Controlls

---
+++ app/src/main/java/com/gyasistory/androids/s3fitness/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.gyasistory.androids.s3fitness.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
